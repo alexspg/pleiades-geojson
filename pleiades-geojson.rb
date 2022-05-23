35adbae22d7d731d429268c92924f1c304ff99d4
Alexander <31589838+alexspg@users.noreply.github.com>
2022-05-23 17:21:23 +0200

    Update pleiades-geojson.rb

--- pleiades-geojson.rb
+++ pleiades-geojson.rb
@@ -4,7 +4,7 @@ Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
 require 'csv'
-require 'json'
+require 'json'./pleiades-geojson.rb pleiades-places-latest.csv pleiades-names-latest.csv pleiades-locations-latest.csv
 
 def locations_to_features(locations)
   features = []
